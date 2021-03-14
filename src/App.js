8894a2da51b6f25d060a476df52459f4392f751c
Ana Knickerbocker <akknickerbocker@spscommerce.com>
2021-03-13 21:30:52 -0600

    Add quotes and set up Prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import QuoteBox from './QuoteBox.jsx'
+import QuoteBox from './QuoteBox.jsx';
 
 const App = () => {
-    return (
-    <div className="App">
-      <QuoteBox author={"Ana"}/>
+  return (
+    <div className='App'>
+      <QuoteBox />
     </div>
   );
-}
+};
 
 export default App;
--- src/QuoteBox.jsx
+++ src/QuoteBox.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 
-const QuoteBox = ({ author }) => {
-    const [quote, setQuote] = React.useState('');
-    const getQuote = async () => {
-        fetch('https://www.affirmations.dev/').then((res) => res.json()).then((data) => setQuote(data.affirmation));
-        console.log(quote)
-        setQuote(quote);
+const getRandomInt = (max) => {
+  const min = 0;
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const QuoteBox = () => {
+  const [quotes, setQuotes] = React.useState([]);
+  const [quote, setQuote] = React.useState('Ana is incredible');
+  const [author, setAuthor] = React.useState(`everyone who's met her`);
+
+  const setupQuotes = async () => {
+    const response = await axios.get('https://type.fit/api/quotes');
+    setQuotes(response.data);
+  };
+
+  const getNextQuote = React.useCallback(() => {
+    if (quotes.length) {
+      const nextQuote = quotes[getRandomInt(quotes.length - 1)];
+      setQuote(nextQuote.text);
+      setAuthor(nextQuote.author);
     }
+  }, [quotes]);
 
-    React.useEffect(() => {
-        getQuote();
-    });
+  React.useEffect(() => {
+    getNextQuote();
+  }, [getNextQuote, quotes]);
 
-    return (
-        <div id="quote-box" className="box-centered">
-            <div id="text">{quote}</div>
-            <div id="author">{author}</div>
-            <button id="get-quote" onClick={getQuote}>Get a new quote</button>
-        </div>
-    );
+  React.useEffect(() => {
+    setupQuotes();
+  }, []);
+
+  return (
+    <div id='quote-box' className='box-centered'>
+      <div id='text'>Quote: {quote}</div>
+      <div id='author'>Author: {author}</div>
+      <button id='get-quote' onClick={getNextQuote}>
+        Get a new quote
+      </button>
+    </div>
+  );
 };
 
 export default QuoteBox;
